43c613ede164087f5495417391354fe903127969
Zero IT Solutions <info@zeroitsolutions.com>
2020-01-22 10:42:36 +0530

    Initial Commit

---
+++ BallerSpotTests/BallerSpotTests.swift
@@ -0,0 +1,34 @@
+//
+//  BallerSpotTests.swift
+//  BallerSpotTests
+//
+//  Created by Zero ITSolutions on 22/01/20.
+//  Copyright © 2020 Zero ITSolutions. All rights reserved.
+//
+
+import XCTest
+@testable import BallerSpot
+
+class BallerSpotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
